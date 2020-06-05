545df5987d32211b720390e10a0a1059975c61a1
Rizardomon <57680110+Rizardomon@users.noreply.github.com>
2020-06-05 01:23:26 -0300

    Ajuste de rotas e arquivos estaticos

--- src/server.js
+++ src/server.js
@@ -1,5 +1,28 @@
 const express = require("express")
 const server = express()
 
+//Configurar pasta publica
+server.use(express.static("public"))
+
+// Utilizando Template Engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+// Configurar caminhos da aplicação
+server.get("/", (req, res) => {
+    return res.render("index.html")
+})
+
+server.get("/create-point", (req, res) => {
+    return res.render("create-point.html")
+})
+
+server.get("/search", (req, res) => {
+    return res.render("search-results.html")
+})
+
 // Ligar o servidor
 server.listen(3000)
